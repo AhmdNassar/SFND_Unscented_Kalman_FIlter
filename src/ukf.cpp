568a862c4c48df5e6746dac8229b0973cddd143e
AhmdNassar <engahmdnassar@gmail.com>
2020-04-23 12:13:17 +0200

    init UKF

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -54,6 +54,12 @@ UKF::UKF() {
    * TODO: Complete the initialization. See ukf.h for other member properties.
    * Hint: one or more values initialized above might be wildly off...
    */
+   n_x_ = 5;
+   n_aug_ = 7;
+   lambda_ = 3 - n_x_;
+   is_initialized_ = false;
+   Xsig_pred_ = MatrixXd(5,15);
+   weights_ = VectorXd(15);
 }
 
 UKF::~UKF() {}
